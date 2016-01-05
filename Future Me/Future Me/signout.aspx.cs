a323f3a8a039fc6f4cdb6aebde3c5a72b9291b05
Shudanti <shudanti@gmail.com>
2016-01-05 21:14:11 +0700

    add signout

---
+++ Future Me/Future Me/signout.aspx.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Future_Me
+{
+    public partial class signout : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            FormsAuthentication.SignOut();
+            FormsAuthentication.RedirectToLoginPage();
+        }
+    }
+}
